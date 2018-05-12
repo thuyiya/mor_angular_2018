45321011b0b833bf8a627d79f608ac03c41de726
thuyiya <thusitha.4t@gmail.com>
2018-05-12 16:20:18 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatButtonModule } from '@angular/material';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
