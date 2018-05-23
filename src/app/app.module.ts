93bc186481ed740066c6202b7f3259f10195eb68
sujeewa <sujeewakabeysinghefacebook@gmail.com>
2018-05-23 21:36:28 +0530

    addSignUpComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     SignInComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule,
