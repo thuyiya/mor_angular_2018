50334b9be66e41f718c0389b94f2ca33845d8b55
sujeewa <sujeewakabeysinghefacebook@gmail.com>
2018-05-23 19:32:46 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignOutComponent } from './user/sign-out/sign-out.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserComponent,
+    SignInComponent,
+    SignOutComponent,
   ],
   imports: [
     BrowserModule,
